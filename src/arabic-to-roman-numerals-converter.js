f52920d408ccd8003b812a2010e3d269890b35fd
dkus <danylo-kushnarov@pluralsight.com>
2017-08-01 15:15:52 +0300

    Awesome code

---
+++ src/arabic-to-roman-numerals-converter.js
@@ -0,0 +1,27 @@
+class ArabicToRomanNumeralsConverter {
+  constructor () {
+    this.lookup = { M:1000, CM:900, D:500, CD:400, C:100, XC:90, L:50, XL:40, X:10, IX:9, V:5, IV:4, I:1 };
+  }
+
+  /**
+   * Code that smells
+   */
+  convert(arabicDigit) {
+    let roman = '';
+
+    if(arabicDigit === 0) {
+      return roman;
+    } else {
+      for (let i in this.lookup ) {
+        while ( arabicDigit >= this.lookup[i] ) {
+          roman += i;
+          arabicDigit -= this.lookup[i];
+        }
+      }
+
+      return roman;
+    }
+  }
+}
+
+module.exports = new ArabicToRomanNumeralsConverter();
---
+++ test/arabic-to-roman-numerals-converter.test.js
@@ -0,0 +1,52 @@
+const chai = require('chai');
+const expect = chai.expect;
+const arabicToRomanNumeralsConverter = require('../src/arabic-to-roman-numerals-converter');
+
+const expects = ({converted, expected}) => {
+  const result = arabicToRomanNumeralsConverter.convert(converted);
+  expect(result).to.equal(expected);
+};
+
+describe('pluralsight engineer coding exercise', () => {
+  describe('arabic to roman numerals converter class', () => {
+    it('does nothing, just a blank test', () => {
+      expect(true).to.equal(true);
+    });
+
+    it('returns an empty string if passed value is 0', () =>
+      expects({converted: 0, expected: ''})
+    );
+
+    it('returns V if passed value is 5', () =>
+      expects({converted: 5, expected: 'V'})
+    );
+
+    it('returns X if passed value is 10', () =>
+      expects({converted: 10, expected: 'X'})
+    );
+
+    it('returns II if passed value is 2', () =>
+      expects({converted: 2, expected: 'II'})
+    );
+
+    it('returns III if passed value is 3', () =>
+      expects({converted: 3, expected: 'III'})
+    );
+
+    it('returns XX if passed value is 20', () =>
+      expects({converted: 20, expected: 'XX'})
+    );
+
+    it('returns L if passed value is 50', () =>
+      expects({converted: 50, expected: 'L'})
+    );
+
+    it('returns IV if passed value is 4', () =>
+      expects({converted: 4, expected: 'IV'})
+    );
+
+    it('returns MMMCMLXXVIII if passed value is 3978', () =>
+      expects({converted: 3978, expected: 'MMMCMLXXVIII'})
+    );
+  });
+});
